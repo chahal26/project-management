b07490355a12822429f6f71dc3b23425909affbf
Sahil Chahal <dev.sahilchahal@gmail.com>
2023-01-04 13:32:07 +0530

    Added User Form

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Enums\TeamEnum;
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('team')
+                    ->options(TeamEnum::getAllValues())
+                    ->required(),
+                Forms\Components\TextInput::make('name')
+                    ->maxLength(255)
+                    ->required(),
+                Forms\Components\TextInput::make('email')
+                    ->maxLength(255)
+                    ->email()
+                    ->required(),
+                Forms\Components\TextInput::make('phone'),
+                Forms\Components\FileUpload::make('image'),
+                Forms\Components\TextInput::make('password')
+                    ->maxLength(255)
+                    ->password()
+                    ->required()
+                    ->hiddenOn('edit')
+                    ->dehydrateStateUsing(fn ($state) => Hash::make($state)),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
